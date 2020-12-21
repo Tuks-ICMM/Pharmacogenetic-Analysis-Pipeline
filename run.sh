16bdfb3e84a6b949c8d7191bf00c0eef538d93cf
Graeme Ford <u14350000@tuks.co.za>
2020-12-21 13:14:19 +0200

    Updated log folder reference

--- run.sh
+++ run.sh
@@ -3,12 +3,10 @@
 #PBS -l walltime=900:00:00
 #PBS -l nodes=1:ppn=14
 #PBS -k oe
-#PBS -o /nlustre/users/graeme/pipeline-2020/Logs/QSUB.log
-#PBS -e /nlustre/users/graeme/pipeline-2020/Logs/QSUB.err
+#PBS -o /nlustre/users/graeme/pipeline-2020/logs/pipeline.log
+#PBS -e /nlustre/users/graeme/pipeline-2020/logs/pipeline.err
 #PBS -N Snakemake Pipeline
 
 module load python-3.8.2
 cd /nlustre/users/graeme/pipeline-2020/
 snakemake --cluster-config cluster.json --profile profile 
-
-# Unable to implement --report for now as it requires the Pytho package `Jinja2`.
