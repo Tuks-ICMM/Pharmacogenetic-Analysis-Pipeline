1df1e0b26ad5692cadbd345bd154af71e73bb185
Graeme Ford <u14350000@tuks.co.za>
2021-02-19 12:35:56 +0200

    Updated run command for MSC Pipeline

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 #PBS -l walltime=900:00:00
 #PBS -l nodes=1:ppn=14
 #PBS -k oe
-#PBS -o /nlustre/users/graeme/pipeline-2020/logs/pipeline.log
-#PBS -e /nlustre/users/graeme/pipeline-2020/logs/pipeline.err
+#PBS -o /nlustre/users/graeme/pipeline-2020/Logs/pipeline.log
+#PBS -e /nlustre/users/graeme/pipeline-2020/Logs/pipeline.err
 #PBS -N Snakemake Pipeline
 
 module load python-3.8.2
-cd /nlustre/users/graeme/pipeline-2020/
+cd /nlustre/users/graeme/pipeline-msc/
 snakemake --cluster-config cluster.json --profile profile 
