c524b94ef3aedc14d87aeb3c2388bc83466a75a5
Graeme Ford <u14350000@tuks.co.za>
2021-02-19 12:38:16 +0200

    Updated run config again...

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -q long
 #PBS -l walltime=900:00:00
-#PBS -l nodes=1:ppn=14
+#PBS -l nodes=1:ppn=1
 #PBS -k oe
-#PBS -o /nlustre/users/graeme/pipeline-2020/Logs/pipeline.log
-#PBS -e /nlustre/users/graeme/pipeline-2020/Logs/pipeline.err
+#PBS -o /nlustre/users/graeme/pipeline-msc/Logs/pipeline.log
+#PBS -e /nlustre/users/graeme/pipeline-msc/Logs/pipeline.err
 #PBS -N Snakemake Pipeline
 
 module load python-3.8.2
