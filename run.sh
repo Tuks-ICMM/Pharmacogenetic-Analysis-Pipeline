13dc3af42b1330db056e4030c0138dd1cf5890cc
Graeme Ford <frd.graeme@gmail.com>
2021-06-06 12:24:18 +0200

    Migrated to conda env

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@
 #PBS -e /nlustre/users/graeme/pipeline-2020/logs/pipeline.err
 #PBS -N Snakemake Pipeline
 
-module load python-3.8.2
+module load anaconda3-2020.07
 cd /nlustre/users/graeme/pipeline-2020/
 snakemake --cluster-config cluster.json --profile profile 
