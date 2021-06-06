5320e8d0843f0c4044a9360c132630dbce739cd8
Graeme Ford <frd.graeme@gmail.com>
2021-06-06 12:19:14 +0200

    Updated run.sh config for MSc

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -q long
-#PBS -l walltime=900:00:00
+#PBS -q short
+#PBS -l walltime=00:30:00
 #PBS -l nodes=1:ppn=1
 #PBS -k oe
-#PBS -o /nlustre/users/graeme/pipeline-msc/Logs/pipeline.log
-#PBS -e /nlustre/users/graeme/pipeline-msc/Logs/pipeline.err
+#PBS -o /nlustre/users/graeme/pipeline-2020/logs/pipeline.log
+#PBS -e /nlustre/users/graeme/pipeline-2020/logs/pipeline.err
 #PBS -N Snakemake Pipeline
 
 module load python-3.8.2
-cd /nlustre/users/graeme/pipeline-msc/
+cd /nlustre/users/graeme/pipeline-2020/
 snakemake --cluster-config cluster.json --profile profile 
