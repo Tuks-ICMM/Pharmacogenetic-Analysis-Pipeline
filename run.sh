fe55ca195cf4e44010fef096825a49ae935e74e8
Graeme Ford <frd.graeme@gmail.com>
2021-07-21 11:45:02 +0200

    Updated run

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@
 #PBS -e /nlustre/users/graeme/pipeline-2020/logs/pipeline.err
 #PBS -N Snakemake Pipeline
 
-module load anaconda3-2020.07
+module load python-3.8.2
 cd /nlustre/users/graeme/pipeline-2020/
-snakemake --cluster-config cluster.json --profile profile 
+snakemake --cluster-config cluster.json --profile config/PBS-Torque-Profile 
