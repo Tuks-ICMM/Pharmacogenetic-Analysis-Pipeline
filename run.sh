4125a027987bc024a6b73115f4501cbfbb41ebee
Graeme Ford <frd.graeme@gmail.com>
2021-07-21 11:48:07 +0200

    Fixed run

--- run.sh
+++ run.sh
@@ -3,10 +3,8 @@
 #PBS -l walltime=900:00:00
 #PBS -l nodes=1:ppn=1
 #PBS -k oe
-#PBS -o /nlustre/users/graeme/pipeline-2020/logs/pipeline.log
-#PBS -e /nlustre/users/graeme/pipeline-2020/logs/pipeline.err
-#PBS -N Snakemake Pipeline
+#PBS -N Snakemake
 
 module load python-3.8.2
 cd /nlustre/users/graeme/pipeline-2020/
-snakemake --cluster-config cluster.json --profile config/PBS-Torque-Profile 
+snakemake --cluster-config config/cluster.json --profile config/PBS-Torque-Profile 
