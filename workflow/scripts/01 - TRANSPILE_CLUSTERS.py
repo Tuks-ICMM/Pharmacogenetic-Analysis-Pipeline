792a90bf7f01992c53ba7a4797dc09448011ef20
G-Kodes <frd.graeme@gmail.com>
2021-11-16 15:55:54 +0200

    Re-Factored PEP support

--- workflow/scripts/01 - TRANSPILE_CLUSTERS.py
+++ workflow/scripts/01 - TRANSPILE_CLUSTERS.py
@@ -16,9 +16,9 @@ __maintainer__ = "Graeme Ford"
 __email__ = "graeme.ford@tuks.co.za"
 __status__ = "Development"
 
-cluster = pd.read_excel(join("config", snakemake.config["cluster"]["file"]))
-cluster["FID"] = cluster["ID"]
-cluster[["ID", "FID", snakemake.wildcards.cluster]].to_csv(
+snakemake.pep.sample_table.reset_index(drop=True)[
+    ["IID", "FID", snakemake.wildcards.cluster]
+].to_csv(
     join("results", "REFERENCE", "cluster_{}.txt".format(snakemake.wildcards.cluster)),
     sep="\t",
     index=False,
