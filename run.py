5589699462eecc386b84163c367cca7b60a9a207
G-Kodes <frd.graeme@gmail.com>
2021-11-16 16:15:15 +0200

    Fixed file path generation

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, abspath
 from json import load
 from subprocess import run
 
@@ -15,7 +15,7 @@ PBS_Headers = [
 
 PBS_Body = [
     "module load python-3.8.2",
-    "cd {};".format(config["environment"]["working-directory"]),
+    "cd {};".format(abspath(__file__)),
     "snakemake --cluster-config config/cluster.json --profile config/PBS-Torque-Profile",
 ]
 
